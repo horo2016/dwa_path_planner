818ca583e54f99992e6a480f7b8e0eadcdff2298
lide <839602830@qq.com>
2020-11-30 16:58:52 +0800

    op

--- dwa_demo.cpp
+++ dwa_demo.cpp
@@ -9,26 +9,27 @@
 #include "utility.h"
 #include "dwa.h"
 
+//20m*20m
 cv::Point2i cv_offset(    float x, float y, int image_width=200, int image_height=200)
 {
     cv::Point2i output;
-    output.x = int(x * 10) + image_width/2;
+    output.x = int(x * 10) ;
     output.y = image_height - int(y * 10) - image_height/3;
     return output;
 };
 
 //如果目标里障碍物太近 极容易导致原地画圈
 int main(){
-    State start{0.0, 0.0, 1, 0.0, 0.0};
-    Point goal{1.0,9.0};
+    State start{10.0, 0.0, 1, 0.0, 0.0};//起始坐标设定在x10m,y1m处
+    Point goal{10.0,9.0};//坐标设定在x10m,y9m处
     Obstacle ob{
                         
-                        {5.0, 4.0},
-                        {5.0, 5.0},
-                       // {2.0, 5.0},
+                        {6.0, 4.0},//1g =10cm
+                        {6.0, 5.0},
+                     //   {2.0, 5.0},
                        // {1.0, 5.0},
-                        {-5.0, 4.0},
-                        {-5.0, 6.0},
+                        {14.0, 4.0},
+                        {14.0, 6.0},
                         
                 };
 
@@ -53,7 +54,7 @@ int main(){
  	cv::VideoWriter writer;
 	writer.open("out_dwa.avi",CV_FOURCC('M', 'J', 'P', 'G'),
         20, //不进行跟踪，定位，只显示、录制时的帧率
-        cv::Size(500,500),
+        cv::Size(200,200),
         true);
     if (!writer.isOpened())
     {
@@ -63,7 +64,7 @@ int main(){
         traj.push_back(x);
 
         // visualization
-        cv::Mat bg(500,500, CV_8UC3, cv::Scalar(255,255,255));
+        cv::Mat bg(200,200, CV_8UC3, cv::Scalar(255,255,255));
         cv::circle(bg, cv_offset(goal.x_, goal.y_, bg.cols, bg.rows),
                    3, cv::Scalar(255,0,0), 5);
 				   
@@ -79,6 +80,7 @@ int main(){
                    3, cv::Scalar(0,0,255), 5);
 // toDegree degree = radian/pi*180;
 		printf("car state:(%.1f,%.1f,%.1f),%.1f \n",x.x_,x.y_,x.theta_,x.theta_/3.14*180);
+		printf("distance goal :%.1f \n",std::sqrt(std::pow((x.x_ - goal.x_), 2) + std::pow((x.y_ - goal.y_), 2)));
         cv::arrowedLine(
                 bg,
                 cv_offset(x.x_, x.y_, bg.cols, bg.rows),
